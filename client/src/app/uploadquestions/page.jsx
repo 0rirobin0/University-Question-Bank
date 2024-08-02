314620f93760c13d8f2b9ce534aa2de436027ab4
robin <rirobink@gmail.com>
2024-08-03 04:08:59 +0600

    fix

--- client/src/app/uploadquestions/page.jsx
+++ client/src/app/uploadquestions/page.jsx
@@ -1,38 +1,9 @@
-import { Button } from "@/components/ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-import { IoIosArrowBack } from "react-icons/io";
+import React from 'react'
 
-export default function uploadquestions() {
+export default function page() {
   return (
     <div>
-      <Drawer direction="left">
-        <DrawerTrigger>
-          {" "}
-          <Button variant="outline">Open</Button>
-        </DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerClose className="w-full flex justify-center items-center">
-              <Button variant="outline">
-                <IoIosArrowBack className="text-md mr-2" />
-                Back
-              </Button>
-            </DrawerClose>
-            <DrawerTitle></DrawerTitle>
-            <DrawerDescription></DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter></DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+      Upload questions
     </div>
-  );
+  )
 }
